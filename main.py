237bcde7c5950c645e574f4141cd841866e2fb44
sardormak <sardorpubg229@gmail.com>
2023-06-27 17:42:00 +0500

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print('poshel nahuy')
-print('i love your mother')
+import pygame
+class Sprite():
+    image = pygame.image.load('assets/images.jpg')
+    speed = 1
+    x = 100
+    y = 100
+
+    def move(self, side):
+        if side == 'left':
+            self.x += self.speed
+        elif side == 'right':
+            self.x -= self.speed
+
+w = pygame.display.set_mode((1279, 700))
+Player = Sprite()
+
+game = True
+while game:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            game = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        Player.x += Player.speed
+    elif keys[pygame.K_LEFT]:
+        Player.x -= Player.speed
+    elif keys[pygame.K_UP]:
+        Player.y -= Player.speed
+
+    elif keys[pygame.K_DOWN]:
+        Player.y += Player.speed
+    w.fill((0, 0, 0))
+    w.blit(Player.image,(Player.x, Player.y))
+    pygame.display.update()
+
+pygame.quit()
