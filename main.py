2cac9bc1a939bd3e7b49c65f0120cc79d0cf7cd9
sardormak <137892233+sardormak@users.noreply.github.com>
2023-07-04 17:21:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame
-class Sprite():
-    image = pygame.image.load('assets/images.jpg')
-    speed = 1
-    x = 100
-    y = 100
+class Sprite:
+    def __init__(self, x, y, speed, img):
+        self.image = pygame.image.load(img).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (200, 200))
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.imgR = self.image
+        self.imgL = pygame.transform.flip(self.image, True, False)
+
+    def cords(self):
+        return tuple([self.x, self.y])
 
     def move(self, side):
-        if side == 'left':
+        if side == 'right':
             self.x += self.speed
-        elif side == 'right':
+            self.image = self.imgL
+        elif side == 'left':
+            self.x -= self.speed
+            self.image = self.imgR
+        elif side == 'up':
+            self.x += self.speed
+        elif side == 'down':
             self.x -= self.speed
 
-w = pygame.display.set_mode((1279, 700))
-Player = Sprite()
 
+w = pygame.display.set_mode((1279, 700))
+Player = Sprite(100, 100, 1, '12345/qwerty.jpg')
 game = True
 while game:
     for ev in pygame.event.get():
@@ -21,16 +34,10 @@ while game:
             game = False
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
-        Player.x += Player.speed
-    elif keys[pygame.K_LEFT]:
-        Player.x -= Player.speed
-    elif keys[pygame.K_UP]:
-        Player.y -= Player.speed
-
-    elif keys[pygame.K_DOWN]:
-        Player.y += Player.speed
+        Player.move('right')
+    if keys[pygame.K_LEFT]:
+        Player.move('left')
     w.fill((0, 0, 0))
-    w.blit(Player.image,(Player.x, Player.y))
+    w.blit(Player.image, Player.cords())
     pygame.display.update()
-
 pygame.quit()
